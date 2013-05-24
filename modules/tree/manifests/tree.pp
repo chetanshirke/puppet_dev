d89fb3b26a588da71b65d8b44f6f368ecf91fa80
root <root@aws-puppet.rsymphony.com>
2013-05-24 10:34:15 +0000

    [puppet_server_dev_test] modules added

--- modules/tree/manifests/tree.pp
+++ modules/tree/manifests/tree.pp
@@ -3,14 +3,11 @@ class tree {
     ensure => installed,
   }
 
-  file { "/tmp/tree_install_successful":
-     owner   => root,
-     group   => root,
-     mode    => 664,
-     ensure  => present,
-     source  => "/etc/puppet/files/tree_install_successful"
+  file { "/tmp/tree_install_successful" :
+    mode => 664,
+    content => template("tree/tree_install_successful.erb")
   }
-  
+
   notify {'Setup TREE':
     message => "TREE installed successfully",
   }
